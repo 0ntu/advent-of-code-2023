bda582c01b240712ec459209f76690857f6fc3bf
Ontu <ontu@gmail.com>
2023-12-15 10:18:11 -0500

    day 7 p2

--- src/zig/test.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    const asdf: *const [4:0]u8 = "asdf";
-    const array = asdf[0..asdf.len].*;
-    array[1] = 'q';
-
-    std.debug.print("{} /// {s} {s}\n", .{@TypeOf(asdf) ,array, asdf});
-}
